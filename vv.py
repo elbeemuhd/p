ddcc843b5540052027d5410c2353cc1f5ef9c3a9
elbeemuhd <48762379+elbeemuhd@users.noreply.github.com>
2019-05-15 14:28:06 +0200

    Update vv.py

--- vv.py
+++ vv.py
@@ -1,19 +1,20 @@
-def isPrime(n)
-    if n < 2; return "Neither prime, nor composite"
-    for i in range(2 , int(n**0.5) + 1):
+def isPrime(n):
+    if n < 2:
+        return "Neither prime, nor composite"
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
-        return True
+    return True
 
 # returns the nth prime number
-def nthPrime (n):
-    numberOfPrime = 0
+def nthPrime(n):
+    numberOfPrimes = 0
     prime = 1
 
-    while numberOfPrime < n:
-        Prime += 1
+    while numberOfPrimes < n:
+        prime += 1
         if isPrime(prime):
-            numberOfPrime +=1
+            numberOfPrimes += 1
     return prime
 
-print(nthprime(10001))
+print(nthPrime(10001))
